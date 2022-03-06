4ddd5c3385ac5c07ce1638ce6fd9108b2f5976d1
Andres Uris <andres.urismontilla@gmail.com>
2022-03-06 14:28:40 -0400

    add greeter program

---
+++ greeter/main.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"io"
+	"os"
+	"fmt"
+	"bufio"
+	"errors"
+	"strconv"
+)
+
+type config struct {
+	numTimes int
+	printUsage bool
+}
+
+var usageMsg = fmt.Sprintf(`Usage: %s <integer> [-h|--help]
+
+A greeter application which prints the name you entered <integer> number of times.
+`, os.Args[0])
+
+func parseArgs(args []string) (config, error) {
+	var numTimes int
+	var err error
+	c := config{}
+
+	if len(args) != 1 {
+		return c, errors.New("Invalid numbers of arguments") 
+	}
+
+	if args[0] == "-h" || args[0] == "--help" {
+		c.printUsage = true
+		return c, nil
+	}
+
+	numTimes, err = strconv.Atoi(args[0])
+
+	if err != nil {
+		return c, err
+	}
+
+	c.numTimes = numTimes
+
+	return c, nil
+}
+
+func printUsage(writer io.Writer) {
+	fmt.Fprintf(writer, usageMsg)
+}
+
+func validateArgs(c config) (error) {
+	if !(c.numTimes > 0) {
+		return errors.New("Must specify a number greater than 0")
+	}
+
+	return nil
+}
+
+func getName(reader io.Reader, writer io.Writer) (string, error) {
+	msg := "Your name please? Press the enter key when done.\n"
+
+	fmt.Fprintf(writer, msg)
+
+	scanner := bufio.NewScanner(reader)
+	scanner.Scan()
+
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+
+	name := scanner.Text()
+
+	if len(name) == 0 {
+		return "", errors.New("You didn't enter your name")
+	}
+
+	return name, nil
+}
+
+func greetUser(c config, name string, writer io.Writer) {
+	msg := fmt.Sprintf("Nice to meet you %s\n", name)
+
+	for i := 0; i < c.numTimes; i++ {
+		fmt.Fprintf(writer, msg)
+	}
+}
+
+func runCmd(reader io.Reader, writer io.Writer, c config) error {
+	if (c.printUsage) {
+		printUsage(writer)
+		return nil
+	}
+
+	name, err := getName(reader, writer)
+
+	if err != nil {
+		return err
+	}
+
+	greetUser(c, name, writer)
+	return nil
+}
+
+
+func main() {
+	c, err := parseArgs(os.Args[1:])
+
+	if err != nil {
+		fmt.Fprintln(os.Stdout, err)
+		printUsage(os.Stdout)
+		os.Exit(1)
+	}
+
+	if c.printUsage {
+		printUsage(os.Stdout)
+		os.Exit(1)
+	}
+
+	err = validateArgs(c)
+
+	if (err != nil) {
+		fmt.Fprintln(os.Stdout, err);
+		os.Exit(1)
+	}
+
+	err = runCmd(os.Stdin, os.Stdout, c)
+
+	if err != nil {
+		fmt.Fprintln(os.Stdout, err)
+		os.Exit(1)
+	}
+}
